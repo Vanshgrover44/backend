8f9b5f9ee0cebdb15d056b60772373a5663c4671
Vanshgrover44 <vanshgrover4321@gmail.com>
2024-09-02 18:14:58 +0530

    connection to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../Constant.js";
+
+const connectdb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `Mongodb Connected !! host connected ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("mongo db error occurred",  error);
+    process.exit(1);
+  }
+};
+
+connectdb();
+export default connectdb;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-// Import the http module using ES Modules syntax
-import http from 'http';
+import dotenv from 'dotenv'
+import connectdb from "./db/index.js";
 
-// Define the hostname and port
-const hostname = '127.0.0.1';
-const port = 3000;
-
-// Create the server
-const server = http.createServer((req, res) => {
-  // Set the response header with HTTP status and content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  // Send a response to the client
-  res.end('Hello, World!\n');
-});
-
-// Start the server and listen on the specified port and hostname
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+dotenv.config({path: './env'})
